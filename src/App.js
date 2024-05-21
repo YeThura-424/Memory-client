36fc8ab7ab4c3d19779919f2a91a50aa65f5b009
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-21 23:07:52 +0630

    Folder Structure and material ui setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memory</Typography>
+      </AppBar>
+    </Container>
+  )
 };
 
 export default App;
