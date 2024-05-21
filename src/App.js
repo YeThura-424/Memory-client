cf17ca7c424a1ae76413b95b95143e90cbe90600
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-21 23:15:11 +0630

    app layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
-
+import memory from './images/images.jpeg';
 const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
         <Typography variant="h2" align="center">Memory</Typography>
+        <img src={memory} alt="memory" height="60" width="80" />
       </AppBar>
+      <Grow in> 
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing="3">
+
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   )
 };
