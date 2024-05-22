a2227973865d8e6b7f8112202569fdcc8e01d622
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-22 16:29:11 +0630

    importing styles

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Post from "./Post/Post.js";
+import useStyles from './style';
+
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
             <h1>POSTS</h1>
