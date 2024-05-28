42889b2e749a5e3e5d7700a3504d9d5a4ac026c0
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-28 21:22:17 +0630

    touch

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import {thunk} from 'redux-thunk';
 import reducers from './reducers';
-import { ThemeProvider } from '@mui/styles';
-import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme();
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById("root"), HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
-      </ThemeProvider>
   </React.StrictMode>
 );
